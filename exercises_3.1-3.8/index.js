c73bb76741f80f224c1d0661d2a1994d55141eb6
jojimanos <manosgeorge7@gmail.com>
2023-08-04 19:39:37 +0300

    exercises 3.7-3.8

--- exercises_3.1-3.8/index.js
+++ exercises_3.1-3.8/index.js
@@ -1,7 +1,12 @@
 const express = require("express")
 const app = express()
+const morgan = require("morgan")
 
 app.use(express.json())
+
+morgan.token('body', (req) => JSON.stringify(req.body) )
+
+app.use(morgan(':method :url :status - :response-time ms :body'))
 
 let persons = [
     {
