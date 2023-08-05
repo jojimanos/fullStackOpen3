fddd79bd562f6d0d369e5e27d1892896ea020d31
jojimanos <manosgeorge7@gmail.com>
2023-08-05 11:54:36 +0300

    exercise 3.11

--- exercises_3.1-3.11/client/src/services.js
+++ exercises_3.1-3.11/client/src/services.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = 'http://localhost:8000/api/persons'
+const baseURL = '/api/persons'
 
 const GetNames = (setPersons, setNotificationMessageText, setError) => {
     const request = axios.get(baseURL)
